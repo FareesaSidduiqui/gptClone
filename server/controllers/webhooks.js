4551e30ea3547b67b72fc00b44cc223cd0282cc5
Fareesa Siddiqui <fareesasiddiqui200pk@gmail.com>
2025-09-15 11:56:49 +0500

    update webhook logic

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,57 +1,56 @@
 import Stripe from 'stripe'
 import Transaction from '../models/transaction.js'
 import User from '../models/User.js'
-import { response } from 'express'
 
+export const stripeWebhooks = async (req, res) => {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+  const sig = req.headers['stripe-signature']
 
-export const stripeWebhooks = async (req,res)=>{
-    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
-    const sig = req.headers['stripe-signature']
+  let event
+  try {
+    event = stripe.webhooks.constructEvent(
+      req.body,
+      sig,
+      process.env.Stripe_Webhook_Signing_key
+    )
+  } catch (error) {
+    return res.status(400).send(`Webhook Error: ${error.message}`)
+  }
 
-    let event
-    try {
-        event = stripe.webhooks.constructEvent(req.body,sig, process.env.Stripe_Webhook_Signing_key)
+  try {
+    switch (event.type) {
+      case 'checkout.session.completed': {
+        const session = event.data.object
+        const { transactionId, appId } = session.metadata
 
-    } catch (error) {
-        return res.status(400).send(`Webhook Error : ${error?.message}`)
-    }
+        if (appId === 'quickgpt') {
+          const transaction = await Transaction.findOne({
+            _id: transactionId,
+            isPaid: false,
+          })
 
-    try {
-        switch (event.type) {
-            case 'payment_intent.succeeded':{
-                const paymentIntent = event.data.object
-                const sessionList = await stripe.checkout.sessions.list({
-                    payment_intent : paymentIntent.id
-                })
+          if (transaction) {
+            // add credits to user
+            await User.updateOne(
+              { _id: transaction.userId },
+              { $inc: { credits: transaction.credits } }
+            )
 
-                const session = sessionList.data[0]
-                const {transactionId , appId} = session.metadata
-
-                if(appId = 'quickgpt'){
-                    const transaction = await Transaction.findOne({_id: 
-                        transactionId , isPaid : false 
-                     })
-                    //  update credits in user acc
-                    await User.updateOne({_id : transaction.userId},{$inc : {
-                        credits : transaction.credits
-                    }}) 
-                    // update credit payment status
-                    transaction.isPaid = true
-                    await transaction.save()
-                }else{
-                    return response.json({received : true, message : 'Ignore event: Invalid app'})
-                }
-                break
-            }
-                        
-            default:
-                console.log('unhandled event type', event.type);
-                
-                break;
+            // mark transaction as paid
+            transaction.isPaid = true
+            await transaction.save()
+          }
         }
-        response.json({received:true})
-    } catch (error) {
-        console.error('Webhook error',error)
-        response.status(500).send('Internal server error', error.message)
+        break
+      }
+
+      default:
+        console.log('Unhandled event type:', event.type)
     }
+
+    res.json({ received: true })
+  } catch (error) {
+    console.error('Webhook error:', error)
+    res.status(500).send(`Internal server error: ${error.message}`)
+  }
 }
